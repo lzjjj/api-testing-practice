dfb934f07378b93607e8358c00443bd7f875d17d
louis <louis.z.j.li@oocl.com>
2018-07-30 11:37:01 +0800

    [louis] complete the test2 of checkCountryForCircuit

--- src/test/java/exercises/RestAssuredExercises2Test.java
+++ src/test/java/exercises/RestAssuredExercises2Test.java
@@ -6,68 +6,73 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class RestAssuredExercises2Test {
 
-	private static RequestSpecification requestSpec;
+    private static RequestSpecification requestSpec;
 
-	@BeforeAll
-	public static void createRequestSpecification() {
+    @BeforeAll
+    public static void createRequestSpecification() {
 
-		requestSpec = new RequestSpecBuilder().
-			setBaseUri("http://localhost").
-			setPort(9876).
-			setBasePath("/api/f1").
-			build();
-	}
-	
-	/*******************************************************
-	 * Use junit-jupiter-params for @ParameterizedTest that
-	 * specifies in which country
-	 * a specific circuit can be found (specify that Monza 
-	 * is in Italy, for example) 
-	 ******************************************************/
+        requestSpec = new RequestSpecBuilder().
+                setBaseUri( "http://localhost" ).
+                setPort( 9876 ).
+                setBasePath( "/api/f1" ).
+                build();
+    }
 
-	//todo
+    /*******************************************************
+     * Use junit-jupiter-params for @ParameterizedTest that
+     * specifies in which country
+     * a specific circuit can be found (specify that Monza
+     * is in Italy, for example)
+     ******************************************************/
 
-	/*******************************************************
-	 * Use junit-jupiter-params for @ParameterizedTest that specifies for all races
-	 * (adding the first four suffices) in 2015 how many  
-	 * pit stops Max Verstappen made
-	 * (race 1 = 1 pitstop, 2 = 3, 3 = 2, 4 = 2)
-	 ******************************************************/
+    //todo
 
-	//todo
+    /*******************************************************
+     * Use junit-jupiter-params for @ParameterizedTest that specifies for all races
+     * (adding the first four suffices) in 2015 how many
+     * pit stops Max Verstappen made
+     * (race 1 = 1 pitstop, 2 = 3, 3 = 2, 4 = 2)
+     ******************************************************/
 
-	/*******************************************************
-	 * Request data for a specific circuit (for Monza this 
-	 * is /circuits/monza.json)
-	 * and check the country this circuit can be found in
-	 ******************************************************/
-	
-	@Test
-	public void checkCountryForCircuit() {
-		
-		given().
-			spec(requestSpec).
-		when().
-		then();
-	}
-	
-	/*******************************************************
-	 * Request the pitstop data for the first four races in
-	 * 2015 for Max Verstappen (for race 1 this is
-	 * /2015/1/drivers/max_verstappen/pitstops.json)
-	 * and verify the number of pit stops made
-	 ******************************************************/
-	
-	@Test
-	public void checkNumberOfPitstopsForMaxVerstappenIn2015() {
-		
-		given().
-			spec(requestSpec).
-		when().
-		then();
-	}
+    //todo
+
+    /*******************************************************
+     * Request data for a specific circuit (for Monza this
+     * is /circuits/monza.json)
+     * and check the country this circuit can be found in
+     ******************************************************/
+
+    @Test
+    public void checkCountryForCircuit() {
+
+        given()
+                .pathParam( "country", "monza" )
+                .spec( requestSpec )
+                .when()
+                .get( "/circuits/{country}.json" )
+                .then()
+                .assertThat()
+                .body( "MRData.CircuitTable.Circuits[0].circuitId", equalTo( "monza" ) );
+    }
+
+    /*******************************************************
+     * Request the pitstop data for the first four races in
+     * 2015 for Max Verstappen (for race 1 this is
+     * /2015/1/drivers/max_verstappen/pitstops.json)
+     * and verify the number of pit stops made
+     ******************************************************/
+
+    @Test
+    public void checkNumberOfPitstopsForMaxVerstappenIn2015() {
+
+        given().
+                spec( requestSpec ).
+                when().
+                then();
+    }
 }
