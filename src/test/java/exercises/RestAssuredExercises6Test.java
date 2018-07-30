3a9e7d8c36ac4c177d054551d26e675d3d73f6fa
louis <louis.z.j.li@oocl.com>
2018-07-30 20:35:30 +0800

    [louis] complete the test6 of checkThatPostingA2012FordFocusReturnsHttp200 and checkThatRetrievingAnAlfaRomeoGiuliaShowsModelYear2016

--- src/test/java/exercises/RestAssuredExercises6Test.java
+++ src/test/java/exercises/RestAssuredExercises6Test.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import dataentities.Address;
+import dataentities.Car;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
@@ -7,6 +9,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class RestAssuredExercises6Test {
 
@@ -26,21 +31,27 @@ public class RestAssuredExercises6Test {
 
 	/*******************************************************
 	 * Create a new Car object that represents a 2012 Ford Focus
-	 * POST this object to /cars/postcar
+	 * POST this object to /car/postcar
 	 * Verify that the response HTTP status code is equal to 200
 	 ******************************************************/
 
 	@Test
 	public void checkThatPostingA2012FordFocusReturnsHttp200() {
 
+		Car car = new Car("Ford", "Focus", 2012);
+
 		given().
-			spec(requestSpec).
-		when().
-		then();
+				spec( requestSpec ).
+				body(car).
+				when().
+				post("/car/postcar").
+				then().
+				assertThat().
+				statusCode(200);
 	}
 
 	/*******************************************************
-	 * Perform a GET to /cars/getcar/alfaromeogiulia
+	 * Perform a GET to /car/getcar/alfaromeogiulia
 	 * Store the response in a Car object
 	 * Verify, using that object, that the model year = 2016
 	 * Use the standard Assert.assertEquals(expected,actual)
@@ -49,11 +60,12 @@ public class RestAssuredExercises6Test {
 
 	@Test
 	public void checkThatRetrievingAnAlfaRomeoGiuliaShowsModelYear2016() {
-
+ 		Car car =
 		given().
 			spec(requestSpec).
-		when();
-
-		// Put your assert here
+				get( "/car/getcar/alfaromeogiulia" ).
+				then().extract().
+				as(Car.class);
+		assertThat(car.getYear(),is(2016));
 	}
 }
